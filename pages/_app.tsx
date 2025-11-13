2c8abddcc7d2ee2dd7b8ff8219a34ff2c19b3ee6
presell <122242589+presell@users.noreply.github.com>
2025-11-12 19:30:51 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,8 @@
 import type { AppProps } from "next/app";
 import Script from "next/script";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      if (typeof window.gtag !== "undefined") {
-        window.gtag("config", "G-NV5QCKX1X7", {
-          page_path: url,
-        });
-      }
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
       {/* Google Analytics */}
@@ -35,8 +15,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
-          window.gtag = gtag;
-
           gtag('js', new Date());
           gtag('config', 'G-NV5QCKX1X7', {
             page_path: window.location.pathname,
