3129d305dfe7195bbbb89c9912df70e29a92027a
presell <122242589+presell@users.noreply.github.com>
2025-11-12 19:34:15 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,41 @@
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Script from "next/script";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (typeof window !== "undefined") {
+        window.gtag?.("config", "G-NV5QCKX1X7", {
+          page_path: url,
+        });
+      }
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
-      {/* Google Analytics */}
+      {/* Google Analytics script loader */}
       <Script
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-NV5QCKX1X7"
       />
 
+      {/* GA initialization + global gtag() function */}
       <Script id="ga-init" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){window.dataLayer.push(arguments);}
+          window.gtag = gtag;
           gtag('js', new Date());
           gtag('config', 'G-NV5QCKX1X7', {
             page_path: window.location.pathname,
