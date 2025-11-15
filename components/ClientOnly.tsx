34fe2410baede2bb1cccfafe1f54ef44ab93a7b1
presell <122242589+presell@users.noreply.github.com>
2025-11-15 02:11:01 -0500

    Update ClientOnly.tsx

--- components/ClientOnly.tsx
+++ components/ClientOnly.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
-export default function ClientOnly({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  // During SSR, and before first client render, render nothing.
-  if (!mounted) return null;
-
+export default function ClientOnly({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <>{children}</>;
 }
