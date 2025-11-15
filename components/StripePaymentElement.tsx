4ce597650c4743d606eef77179df6cd423048f4e
presell <122242589+presell@users.noreply.github.com>
2025-11-14 23:57:10 -0500

    Update StripePaymentElement.tsx

--- components/StripePaymentElement.tsx
+++ components/StripePaymentElement.tsx
@@ -30,7 +30,7 @@ function CheckoutForm() {
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: `${window.location.origin}/payment-success`,
+        return_url: `${window.location.origin}/confirmation`,
       },
     });
 
