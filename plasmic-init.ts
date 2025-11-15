672ad7f96ebc07d219522f2167dcf1619339398f
presell <122242589+presell@users.noreply.github.com>
2025-11-14 23:50:03 -0500

    Update plasmic-init.ts

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -1,24 +1,15 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
-import StripePaymentElement from "./components/StripePaymentElement";
 
 export const PLASMIC = initPlasmicLoader({
   projects: [
     {
       id: "4jNtNf7ennmHcnVPPcPauY",
-      token: "6so33r9xTOt0c2tdo9yC8OoZDfPjnn1E7hbQoRe5f5iPAdvb0Kk3FatMcU9BA6wgT8z8ABEb3ZcxTZ3uNg",
+      token:
+        "6so33r9xTOt0c2tdo9yC8OoZDfPjnn1E7hbQoRe5f5iPAdvb0Kk3FatMcU9BA6wgT8z8ABEb3ZcxTZ3uNg",
     },
   ],
   preview: false,
 });
 
-// Register the Stripe component
-PLASMIC.registerComponent(StripePaymentElement, {
-  name: "StripePaymentElement",
-  props: {
-    amount: {
-      type: "number",
-      defaultValue: 44.9,
-    },
-    className: "string",
-  },
-});
+// DO NOT register Stripe here (SSR) — it breaks Plasmic
+// Registration happens client-side inside /plasmic-host.tsx
