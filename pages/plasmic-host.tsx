594af17080ab7bee6b8eb7ec9118a4c57f318856
presell <122242589+presell@users.noreply.github.com>
2025-11-15 01:05:45 -0500

    Update plasmic-host.tsx

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,28 +1,7 @@
 "use client";
 
-import * as React from "react";
 import { PlasmicCanvasHost } from "@plasmicapp/loader-nextjs";
-import { PLASMIC } from "@/plasmic-init";
 
 export default function PlasmicHost() {
-  React.useEffect(() => {
-    // Register StripePaymentElement only in the browser
-    async function register() {
-      if (typeof window === "undefined") return;
-
-      const mod = await import("@/components/StripePaymentElement");
-
-      PLASMIC.registerComponent(mod.default, {
-        name: "StripePaymentElement",
-        props: {
-          amount: "number",
-          className: "string",
-        },
-      });
-    }
-
-    register();
-  }, []);
-
   return <PlasmicCanvasHost />;
 }
